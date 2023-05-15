0bf6051a19ad9621f5ccf55b451311e8869da33e
Anthonie <Anthonie@DESKTOP-MLNJEPP>
2023-05-15 13:27:54 +0300

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fiche_d_inscription.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
